4421c05be0c395bdffa16efa806d552fa43b04fb
estercalveras <estercalveras@gmail.com>
2023-10-26 17:53:46 +0200

    link added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
 
 library.add(faChevronDown);
 
@@ -16,12 +17,14 @@ function SideBar({ goToNext, currentIndex, firstIndex }) {
 
     return (
         <div className="flex flex-col text-memes-dark-gray justify-between w-full">
-            <div className="flex flex-col justify-start items-start">
-                <div className=" font-nurito text-xl font-black">THE</div>
-                <div className=" font-nurito text-xl font-black">MEME</div>
-                <div className=" font-nurito text-xl font-black">GEN-</div>
-            </div>           
-             {currentIndex !== firstIndex - 1 ?
+            <Link className="" to="/">
+                <div className="flex flex-col justify-start items-start">
+                    <div className=" font-nurito text-xl font-black">THE</div>
+                    <div className=" font-nurito text-xl font-black">MEME</div>
+                    <div className=" font-nurito text-xl font-black">GEN-</div>
+                </div>
+            </Link>
+            {currentIndex !== firstIndex - 1 ?
                 <div onClick={() => goToNext()} className="rounded-xl flex justify-center self-center items-center h-10 w-10 bg-memes-gray hover:bg-memes-light-purple hover:text-memes-purple">
                     <FontAwesomeIcon className="" icon="chevron-down" />
                 </div> :
